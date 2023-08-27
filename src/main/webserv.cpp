dd9ef01d2cb9b1a6e85f8c4b1ebf5cbf2db3b4db
dghonyan <you@example.com>
2023-08-27 20:26:05 +0400

    about error pages

--- src/main/webserv.cpp
+++ src/main/webserv.cpp
@@ -1,22 +1,26 @@
 #include "webserv.hpp"
 
-void loop(Config& conf);
+void loop(Config &conf);
+
+// note about error pages, nginx doesn't use <meta> when error page starts with a slash,
+// but appends it to the request and redirects if it contains any other slashes
+// and redirects without appending when there's no slash
+// it searches for (root + route + error_page)
 
 int main(int argc, char **argv)
 {
-(void)argc;
-(void)argv;
+	(void)argc;
+	(void)argv;
 
+	try
+	{
+		Config conf = (argc > 1 ? Config(argv[1]) : Config());
 
-try
-{
-	Config conf = ( argc > 1 ? Config(argv[1]) : Config() );
-
-	// conf.getLocationData("127.0.0.1", "8080", "server2", "/a").printEverything("");
-	loop(conf);
-}
-catch(const std::exception& e)
-{
-	std::cerr << "Error: " << e.what() << '\n';
-}
+		// conf.getLocationData("127.0.0.1", "8080", "server2", "/a").printEverything("");
+		loop(conf);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << '\n';
+	}
 }
